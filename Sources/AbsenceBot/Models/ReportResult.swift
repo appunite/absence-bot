4e9bfc6f304132ba45ebad0ae0ac909eefe8c37c
Emil <emil@appunite.com>
2019-03-01 10:17:25 +0100

    fix end date in report

--- Sources/AbsenceBot/Models/ReportResult.swift
+++ Sources/AbsenceBot/Models/ReportResult.swift
@@ -25,10 +25,22 @@ public struct ReportResult: Encodable {
     self.eventLink = event.htmlLink
     self.createAt = event.created
     self.updatedAt = event.updated
-    
+
+    // get start day
     self.start = event.start.date ?? event.start.dateTime
-    self.end = event.end.date ?? event.end.dateTime
-    
+
+    // get end day if date-time
+    if let _end = event.end.dateTime {
+      self.end = _end
+    }
+
+    // get end day if whole day event
+    else if let _end = event.end.date {
+      // if we're adding all day event, google is exluding end date, so we need to go back in time by one day
+      let calendar = Calendar.gmtTimeZoneCalendar
+      self.end = calendar.date(byAdding: .day, value: -1, to: _end)
+    }
+
     self.reason = event.summary
       .split(separator: "-")
       .last?
