bc02f598fb6cb60c51844965e4092bbe41f3795f
emil <emil.wojtaszek@gmail.com>
2019-04-01 21:56:15 +0200

    add besic report snapshot test

--- Tests/AbsenceBotTests/ReportMiddlewareTests.swift
+++ Tests/AbsenceBotTests/ReportMiddlewareTests.swift
@@ -1,32 +1,25 @@
-//
-//  ReportMiddlewareTests.swift
-//  AbsenceBotTests
-//
-//  Created by Emil Wojtaszek on 01/04/2019.
-//
-
 import XCTest
+import Either
+import Html
+import HttpPipeline
+import Prelude
+import Optics
+import SnapshotTesting
+import AbsenceBotTestSupport
+@testable import AbsenceBot
 
-class ReportMiddlewareTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+class ReportMiddlewareTests: TestCase {
+  override func setUp() {
+    super.setUp()
+    //    record = true
+  }
+  
+  func testBasicReportGeneration() {
+    Current = .mock
+    
+    let req = request(to: .report(year: 2019, month: 3), basicAuth: true)
+    let conn = connection(from: req)
+    
+    assertSnapshot(matching: conn |> appMiddleware, as: .ioConn)
+  }
 }
